c8a658b241aed745b50bd1ca9c8f1a4de1c1c097
Víctor Cardozo <vcardozof@gmail.com>
2021-06-16 10:11:26 +0200

    Adds oopClass method call in the main

--- src/com/ironhack/main/Main.java
+++ src/com/ironhack/main/Main.java
@@ -11,8 +11,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        fileClass();
+        System.out.println("Hello world :D");
+        oopClass();
 
     }
 
